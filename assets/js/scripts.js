87418a589ced8df977f62d2025bdbcc29ca486a0
qwe <taroz@telegmail.com>
2021-05-24 01:34:50 +0800

    perfect

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -64,7 +64,17 @@
         $(this).removeClass("floating-label-form-group-with-focus");
       });
     });
-    
+
+    function getUrlParam(name)
+    {
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r!=null) return unescape(r[2]); return null; //返回参数值
+    }
+    console.log(getUrlParam('eth'));
+    var t = 'https://catcoin.pages.dev/?eth='+getUrlParam('eth');
+    $('#myurl').val(t);
+    $("#copyurl").attr("data-clipboard-text",t);
   })(jQuery); // End of use strict
 
   function initParticles(){
@@ -186,7 +196,7 @@
 $(document).ready(function(){
 
   initParticles();
-  
 
-});
+}
+);
 
